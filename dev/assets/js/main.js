ecfeca11906c536be3319c160ca34fc965a4d9f1
Jozef Maxted <jozef.maxted@theunit.co.uk>
2016-08-27 18:25:44 +0100

    Add more work

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -219,6 +219,22 @@
 
     $( window ).on( 'scroll', throttle( setActiveSection, 250 ) );
 
+    // Smooth scroll
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 } )();
 
 ( function() {
